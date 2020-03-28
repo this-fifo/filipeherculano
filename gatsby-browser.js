5a0fbd349ab4058f04c56ccda96065738ac92a7d
Filipe Herculano <fifo_@live.com>
2020-03-28 18:06:59 -0400

    Change code highlighting theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 // gatsby-browser.js
 import "./src/pages/styles/global.css"
-require("prismjs/themes/prism-solarizedlight.css")
+require("prism-themes/themes/prism-dracula.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
